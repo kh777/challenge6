6f017b39b01b59f194a2db13ddd6dc208bbde538
Koping Hu <kh777@yahoo.com>
2013-11-06 00:38:19 -0500

    solved the challenge

---
+++ CCTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  CCTableViewController.h
+//  UITableViewController Challenge Solution
+//
+//  Created by KP on 11/6/13.
+//  Copyright (c) 2013 KH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCTableViewController : UITableViewController
+
+@end
